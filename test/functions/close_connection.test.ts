d469b65fa44edec23b8bde50939c04360b0139d6
Yongkyun Lee <yongkyun.daniel.lee@gmail.com>
2022-01-14 03:08:31 -0500

    Updated CREATE TABLE postgres for tests

--- test/functions/close_connection.test.ts
+++ test/functions/close_connection.test.ts
@@ -61,8 +61,8 @@ describe("AWS Lambda function - close_connection", () => {
             CREATE TABLE IF NOT EXISTS friendship_table (
                 user_id1     VARCHAR(50),
                 user_id2     VARCHAR(50),
-                requested_at DATETIME,
-                accepted_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                requested_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                accepted_at  TIMESTAMP,
                 PRIMARY KEY (user_id1, user_id2)
             );`;
         await pgClient.query(text);
